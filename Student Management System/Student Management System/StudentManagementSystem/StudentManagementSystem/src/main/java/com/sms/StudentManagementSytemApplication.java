384867d12e2c98a7f829e5de09b4a44194936b1c
Dhanashree Durge <104904004+Dhanshreedurge@users.noreply.github.com>
2023-12-12 00:07:56 +0530

    Add files via upload

---
+++ Student Management System/Student Management System/StudentManagementSystem/StudentManagementSystem/src/main/java/com/sms/StudentManagementSytemApplication.java
@@ -0,0 +1,16 @@
+package com.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class StudentManagementSytemApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSytemApplication.class, args);
+		System.out.println("Welcome to Student Management system");
+	}
+
+}
---
+++ Student Management System/Student Management System/StudentManagementSystem/StudentManagementSystem/src/main/java/com/sms/serviceimpl/TeacherServiceImpl.java
@@ -0,0 +1,53 @@
+package com.sms.serviceimpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sms.entities.Teacher;
+import com.sms.repository.TeacherRepository;
+import com.sms.service.TeacherService;
+
+@Service
+public class TeacherServiceImpl implements TeacherService{
+
+	@Autowired
+	private TeacherRepository teacherRepository;
+	
+	@Override
+	public Teacher createTeacher(Teacher teacher) {
+		// TODO Auto-generated method stub
+		return teacherRepository.save(teacher);
+	}
+
+	@Override
+	public List<Teacher> getAllTeachers() {
+		
+		return teacherRepository.findAll();
+	}
+
+	@Override
+	public Teacher updateTeacherById(int id, Teacher teacher) {
+		Teacher t=teacherRepository.findById(id).get();
+		t.setName(teacher.getName());
+		t.setAge(teacher.getAge());
+		t.setSubject(teacher.getSubject());
+		return teacherRepository.save(t);
+	
+	}
+
+	@Override
+	public Teacher getTeacherById(int id) {
+	
+		return teacherRepository.findById(id).get();
+	}
+
+	@Override
+	public void deleteTeacherById(int id) {
+
+		
+		 teacherRepository.deleteById(id);;
+	
+	}
+}
